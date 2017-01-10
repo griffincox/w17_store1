0008fe559532a77496015bb018eb023d2183fca7
Raghu Betina <raghu@firstdraft.com>
2017-01-10 19:56:31 +0000

    Only address user can modify address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,4 +1,14 @@
 class AddressesController < ApplicationController
+  before_action :current_user_must_be_address_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_address_user
+    address = Address.find(params[:id])
+
+    unless current_user == address.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @addresses = Address.all
 
