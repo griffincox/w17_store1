d4743e5fe1933f661615de976caecccdf22f3f1c
Raghu Betina <raghu@firstdraft.com>
2017-01-10 19:55:55 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "addresses#index"
   # Routes for the Productitem resource:
   # CREATE
